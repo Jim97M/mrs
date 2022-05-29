419bb73b840725f474bba0ca9c02bdd01c2c3b15
Jim97M <wabuyajames@gmail.com>
2022-05-29 19:33:20 +0300

    Bootstrap Mysql Connect

---
+++ src/test/java/com/projects/management/ManagementApplicationTests.java
@@ -0,0 +1,14 @@
+package com.projects.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
